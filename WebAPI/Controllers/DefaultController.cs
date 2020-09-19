a23411d21f76baa65a01005b6b1befc341c16a07
superfeeling-git <58456025+superfeeling-git@users.noreply.github.com>
2020-09-19 22:59:57 +0800

    添加项目文件。

---
+++ WebAPI/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace WebAPI.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Login(string UserName, string Password)
+        {
+            FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, UserName, DateTime.Now, DateTime.Now.AddDays(1), true, $"{UserName}:{Password}");
+
+            string _ticket = FormsAuthentication.Encrypt(ticket);
+
+            HttpRuntime.Cache["User"] = new User { UserName = UserName, Password = Password };
+
+            return Ok<string>(_ticket);
+        }
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
