cdec524c887f4e9192260fc4ba37118f050c8f3d
superfeeling-git <58456025+superfeeling-git@users.noreply.github.com>
2020-09-20 12:05:05 +0800

    Oauth2验证

--- OauthAPI/App_Start/WebApiConfig.cs
+++ OauthAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace OauthAPI
 {
@@ -9,6 +10,9 @@ namespace OauthAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            //跨域配置
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
             // Web API 配置和服务
 
             // Web API 路由
